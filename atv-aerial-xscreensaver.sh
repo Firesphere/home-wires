ea4b31dd63339c484f108e8d9a0bffa55e03da68
Simon `Firesphere` <firesphere@casa-laguna.net>
2022-05-03 17:05:55 +1200

    Remove enforced video output for smoother playback on all architectures

--- atv-aerial-xscreensaver.sh
+++ atv-aerial-xscreensaver.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+command -v mplayer >/dev/null 2>&1 || {
+  echo "I require mplayer but it's not installed. Aborting." >&2
+  exit 1; 
+}
+
 [[ -z "$XDG_CONFIG_HOME" ]] &&
   XDG_CONFIG_HOME="$HOME/.config"
 
-command -v mplayer >/dev/null 2>&1 || {
-  echo "I require mplayer but it's not installed. Aborting." >&2
-  exit 1; }
+# database files to allow for no repeats when playing videos
+day_db=$XDG_CONFIG_HOME/.atv4-day
+night_db=$XDG_CONFIG_HOME/.atv4-night
 
 # path of movies
 # movies=/opt/ATV4
@@ -146,10 +151,6 @@ CoralsArray=(
     Underseas/Corals/RS_A008_C010_SDR_20191218_SDR_2K_HEVC.mov
 )
 
-# database files to allow for no repeats when playing videos
-day_db=$XDG_CONFIG_HOME/.atv4-day
-night_db=$XDG_CONFIG_HOME/.atv4-night
-
 # Pick the videos you want
 DayArray=(
     "${DayLosAngelesArray[@]}"
@@ -224,26 +225,27 @@ selectVideo() {
         pick=$((RANDOM%length+1))
     fi
     selected=$(sed -n "$pick p" "$used")
-    # Rebuild the list if needed.
+    # Rebuild the list if needed. We do this after selection
+    # So further down, the selected is removed from the newly build list
     buildlist
     
     # Remove the selected video from the "database" files, so we won't repeat
-    $(sed -i "s|$selected|""|g" "$used") # Replace with empty string in the unused db
+    $(sed -i "s|$selected|""|g" "$used") # Replace with empty string in the used db
     $(sed -i '/^$/d' "$used") # Then, remove all empty lines, to keep the line count correct
     # This will happily do nothing if it's not found, so... yay?
     $(sed -i "s|$selected|""|g" "$unused") # Replace with empty string in the unused db
     $(sed -i '/^$/d' "$unused") # Then, remove all empty lines, to keep the line count correct
 }
 
-# https://github.com/kevincox/xscreensaver-videos
 # Because we put everything in newlines in the buildlist, use newlines as the separator
 IFS=$'\n'
+# https://github.com/kevincox/xscreensaver-videos
 trap : SIGTERM SIGINT SIGHUP
 while (true) #!(keystate lshift)
 do
   selectVideo
   # Default, use mplayer
-  /usr/bin/mplayer -vo x11 -nosound -really-quiet -nolirc -nostop-xscreensaver -wid "$XSCREENSAVER_WINDOW" -fs "$movies/$selected" &
+  /usr/bin/mplayer -nosound -really-quiet -nolirc -nostop-xscreensaver -wid "$XSCREENSAVER_WINDOW" -fs "$movies/$selected" &
   # Option 2, use MPV
   #/usr/bin/mpv --really-quiet --no-audio --fs --no-stop-screensaver --wid="$XSCREENSAVER_WINDOW" --panscan=1.0 "$movies/$useit" &
   # Option 3, use VLC
