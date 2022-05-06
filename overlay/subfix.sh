613fdb107a70ccc41241a6ccf4eb81b2c3f6bcb9
Simon `Firesphere` <firesphere@casa-laguna.net>
2022-05-06 22:43:48 +1200

    What I'm doing here, I don't know

--- overlay/subfix.sh
+++ overlay/subfix.sh
@@ -4,8 +4,11 @@ srts=$(find . -name "*.srt")
 
 for srt in $srts
 do
+  # nbsp thing
   $(sed -i 's/ / /g' $srt)
+  # weird quotes
   $(sed -i "s/‘/'/g" $srt)
   $(sed -i "s/’/'/g" $srt)
+  # macrons don't work on subtitles
   $(sed -i "s/ā/a/g" $srt)
 done
