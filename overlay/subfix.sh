02e6e9f1b202c9b57936a824045e4d6efacb51df
Simon `Firesphere` <firesphere@casa-laguna.net>
2022-05-07 12:55:15 +1200

    Some more special character removals

--- overlay/subfix.sh
+++ overlay/subfix.sh
@@ -4,11 +4,15 @@ srts=$(find . -name "*.srt")
 
 for srt in $srts
 do
-  # nbsp thing
+  echo "Fixing up $srt"
+  # nbsp thing, needs manual pasting every time
   $(sed -i 's/ / /g' $srt)
-  # weird quotes
-  $(sed -i "s/‘/'/g" $srt)
-  $(sed -i "s/’/'/g" $srt)
-  # macrons don't work on subtitles
-  $(sed -i "s/ā/a/g" $srt)
+  # # Special characters don't work on subtitles
+  i=0
+  find="’‘ÁáÀàÂâǍǎĂăÃãẢảẠạÄäÅåĀāĄąẤấẦầẪẫẨẩẬậẮắẰằẴẵẲẳẶặǺǻĆćĈĉČčĊċÇçĎďĐđÐÉéÈèÊêĚěĔĕẼẽẺẻĖėËëĒēĘęẾếỀềỄễỂểẸẹỆệĞğĜĝĠġĢģĤĥĦħÍíÌìĬĭÎîǏǐÏïĨĩĮįĪīỈỉỊịĴĵĶķĹĺĽľĻļŁłĿŀŃńŇňÑñŅņÓóÒòŎŏÔôỐốỒồỖỗỔổǑǒÖöŐőÕõØøǾǿŌōỎỏƠơỚớỜờỠỡỞởỢợỌọỘộṔṕṖṗŔŕŘřŖŗŚśŜŝŠšŞşŤťŢţŦŧÚúÙùŬŭÛûǓǔŮůÜüǗǘǛǜǙǚǕǖŰűŨũŲųŪūỦủƯưỨứỪừỮữỬửỰựỤụẂẃẀẁŴŵẄẅÝýỲỳŶŷŸÿỸỹỶỷỴỵŹźŽžŻż"
+  replace="''AaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaCcCcCcCcCcDdDdDEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeGgGgGgGgHhHhIiIiIiIiIiIiIiIiIiIiIiJjKkLlLlLlLlLlNnNnNnNnOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoPpPpRrRrRrSsSsSsSsTtTtTtUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuWwWwWwWwYyYyYyYyYyYyYyZzZzZz"
+  for (( i=0; i<${#find}; i++ )); do
+    $(sed -i "s|${find:$i:1}|${replace:$i:1}|g" "$srt")
+  done
 done
+
